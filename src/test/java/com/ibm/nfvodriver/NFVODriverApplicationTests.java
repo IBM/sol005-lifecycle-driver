9fe5d3c6c5afb7f6c4a7a073c7e6abc94096a65d
rkumar8j <rkumar8j@in.ibm.com>
2022-09-08 19:35:30 +0530

    Removing unused comments
    Signed-off-by: rkumar8j <rkumar8j@in.ibm.com>

--- src/test/java/com/ibm/nfvodriver/NFVODriverApplicationTests.java
+++ src/test/java/com/ibm/nfvodriver/NFVODriverApplicationTests.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-/*@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")*/
 @ContextConfiguration( loader = AnnotationConfigContextLoader.class)
 public class NFVODriverApplicationTests{
 
