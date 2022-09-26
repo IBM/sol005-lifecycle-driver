4e0fa2f0249cb0a5c66f41cda400813e11ce5171
rkumar8j <rkumar8j@in.ibm.com>
2022-09-26 15:06:14 +0530

    Code optimization- Adding switch case for multiple conditions
    Signed-off-by: rkumar8j <rkumar8j@in.ibm.com>

--- src/main/java/com/ibm/nfvodriver/service/LifecycleManagementService.java
+++ src/main/java/com/ibm/nfvodriver/service/LifecycleManagementService.java
@@ -1,10 +1,8 @@
 package com.ibm.nfvodriver.service;
 
-import com.ibm.common.utils.LoggingUtils;
+import com.google.common.collect.ImmutableMap;
 import com.ibm.nfvodriver.config.NFVODriverProperties;
 import com.ibm.nfvodriver.driver.NSLifecycleManagementDriver;
-import com.ibm.nfvodriver.model.MessageDirection;
-import com.ibm.nfvodriver.model.MessageType;
 import com.ibm.nfvodriver.model.alm.ExecutionAcceptedResponse;
 import com.ibm.nfvodriver.model.alm.ExecutionAsyncResponse;
 import com.ibm.nfvodriver.model.alm.ExecutionRequest;
@@ -12,7 +10,6 @@ import com.ibm.nfvodriver.model.alm.ExecutionStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -30,6 +27,14 @@ public class LifecycleManagementService {
     private final MessageConversionService messageConversionService;
     private final ExternalMessagingService externalMessagingService;
     private final NFVODriverProperties properties;
+    private final Map requestTypeMap = Map.of(LIFECYCLE_CREATE,"CreateNsRequest",
+            LIFECYCLE_INSTALL, "InstantiateNsRequest",
+            LIFECYCLE_UPGRADE, "UpdateNsRequest",
+            LIFECYCLE_UNINSTALL, "TerminateNsRequest",
+            LIFECYCLE_SCALEIN, "ScaleNsRequest",
+            LIFECYCLE_SCALEOUT, "ScaleNsRequest",
+            LIFECYCLE_SCALETOLEVEL, "ScaleNsRequest",
+            LIFECYCLE_HEAL, "HealNsRequest");
 
     @Autowired
     public LifecycleManagementService(NSLifecycleManagementDriver nsLifecycleManagementDriver, MessageConversionService messageConversionService, ExternalMessagingService externalMessagingService,
@@ -44,13 +49,14 @@ public class LifecycleManagementService {
         logger.info("Processing execution request");
         String lifecycleName = executionRequest.getLifecycleName();
         final String requestId = UUID.randomUUID().toString();
+        String requestPayload = null;
+        if(requestTypeMap.containsKey(lifecycleName))
+            requestPayload = messageConversionService.generateMessageFromRequest((String) requestTypeMap.get(lifecycleName), executionRequest);
         try {
             switch (lifecycleName) {
                 case LIFECYCLE_CREATE:
-                    // Generate CreateNSRequest message
-                    final String createNsRequest = messageConversionService.generateMessageFromRequest("CreateNsRequest", executionRequest);
                     // Send message to NFVO
-                    final String nsInstanceResponse = nsLifecycleManagementDriver.createNsInstance(executionRequest.getDeploymentLocation(), createNsRequest, requestId);
+                    final String nsInstanceResponse = nsLifecycleManagementDriver.createNsInstance(executionRequest.getDeploymentLocation(), requestPayload, requestId);
                     // Convert response into properties to be returned to ALM
                     final Map<String, Object> outputs = messageConversionService.extractPropertiesFromMessage("NsInstance", executionRequest, nsInstanceResponse);
                     // Delay sending the asynchronous response (from a different thread) as this method needs to complete first (to send the response back to Brent)
@@ -59,14 +65,12 @@ public class LifecycleManagementService {
                 case LIFECYCLE_INSTALL:
                     // Instantiate
                     final String nsInstallInstanceId = executionRequest.getStringResourceProperty("nsInstanceId");
-                    final String instantiateNsRequest = messageConversionService.generateMessageFromRequest("InstantiateNsRequest", executionRequest);
-                    final String responseGetInstantiateNsUUID = nsLifecycleManagementDriver.instantiateNs(executionRequest.getDeploymentLocation(), nsInstallInstanceId, instantiateNsRequest);
+                    final String responseGetInstantiateNsUUID = nsLifecycleManagementDriver.instantiateNs(executionRequest.getDeploymentLocation(), nsInstallInstanceId, requestPayload);
                     return new ExecutionAcceptedResponse(responseGetInstantiateNsUUID);
                 case LIFECYCLE_UPGRADE:
                     // Upgrade
                     final String nsUpgradeInstanceId = executionRequest.getStringResourceProperty("nsInstanceId");
-                    final String updateNsRequest = messageConversionService.generateMessageFromRequest("UpdateNsRequest", executionRequest);
-                    final String responseGetUpdateNsUUID = nsLifecycleManagementDriver.updateNs(executionRequest.getDeploymentLocation(), nsUpgradeInstanceId, updateNsRequest);
+                    final String responseGetUpdateNsUUID = nsLifecycleManagementDriver.updateNs(executionRequest.getDeploymentLocation(), nsUpgradeInstanceId, requestPayload);
                     return new ExecutionAcceptedResponse(responseGetUpdateNsUUID);
                 case LIFECYCLE_DELETE:
                     // Delete
@@ -77,22 +81,19 @@ public class LifecycleManagementService {
                 case LIFECYCLE_UNINSTALL:
                     // Terminate
                     final String nsUninstallInstanceId = executionRequest.getStringResourceProperty("nsInstanceId");
-                    final String terminateNsRequest = messageConversionService.generateMessageFromRequest("TerminateNsRequest", executionRequest);
-                    final String responseGetTerminateNsUUID = nsLifecycleManagementDriver.terminateNs(executionRequest.getDeploymentLocation(), nsUninstallInstanceId, terminateNsRequest);
+                    final String responseGetTerminateNsUUID = nsLifecycleManagementDriver.terminateNs(executionRequest.getDeploymentLocation(), nsUninstallInstanceId, requestPayload);
                     return new ExecutionAcceptedResponse(responseGetTerminateNsUUID);
                 case LIFECYCLE_SCALEIN:
                 case LIFECYCLE_SCALEOUT:
                 case LIFECYCLE_SCALETOLEVEL:
                     // ScaleIn/ScaleOut/ScaleToLevel
                     final String nsScaleInstanceId = executionRequest.getStringResourceProperty("nsInstanceId");
-                    final String scaleNsRequest = messageConversionService.generateMessageFromRequest("ScaleNsRequest", executionRequest);
-                    final String responseGetScaleNsUUID = nsLifecycleManagementDriver.scaleNs(executionRequest.getDeploymentLocation(), nsScaleInstanceId, scaleNsRequest);
+                    final String responseGetScaleNsUUID = nsLifecycleManagementDriver.scaleNs(executionRequest.getDeploymentLocation(), nsScaleInstanceId, requestPayload);
                     return new ExecutionAcceptedResponse(responseGetScaleNsUUID);
                 case LIFECYCLE_HEAL:
                     // Heal
                     final String nsHealInstanceId = executionRequest.getStringResourceProperty("nsInstanceId");
-                    final String healNsRequest = messageConversionService.generateMessageFromRequest("HealNsRequest", executionRequest);
-                    final String responseGetHealNsUUID = nsLifecycleManagementDriver.healNs(executionRequest.getDeploymentLocation(), nsHealInstanceId, healNsRequest);
+                    final String responseGetHealNsUUID = nsLifecycleManagementDriver.healNs(executionRequest.getDeploymentLocation(), nsHealInstanceId, requestPayload);
                     return new ExecutionAcceptedResponse(responseGetHealNsUUID);
                 default:
                     throw new IllegalArgumentException(String.format("Requested transition [%s] is not supported by this lifecycle driver", executionRequest.getLifecycleName()));
@@ -102,5 +103,4 @@ public class LifecycleManagementService {
             throw e;
         }
     }
-
 }
